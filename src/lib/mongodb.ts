456f23a782e5f1eec3c57ffa8a1bb4b20cdd3186
Naitsabes1955 <juansebasmosquera519@gmail.com>
2026-06-03 13:57:24 -0500

    "refactor: validation of env variables"

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,14 +1,21 @@
 import mongoose from "mongoose";
 
+ const MONGO_URI = process.env.MONGO_URI!;
+
+ if(!MONGO_URI){
+  throw new Error("There aren't enviorement variable")
+ }
+
 export async function connectDB() {
 
   try {
-    const MONGO_URI = process.env.MONGO_URI;
-
-    await mongoose.connect(MONGO_URI!);
+   
+     
+    await mongoose.connect(MONGO_URI);
 
     console.log("MongoDB connected");
   } catch (error) {
-    console.error(error);
+    console.error(error)
+    throw error
   }
 }
